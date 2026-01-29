3693a24ed6f5117cb7f5d99276f66f86a526b63f
danilotec <danilo0123499@gmail.com>
2026-01-29 16:46:33 -0300

    telebot

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import telebot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+API_TOKEN_TELEBOT = os.getenv("TELEGRAM_TOKEN", '')
+CHAT_ID = os.getenv('CHAT_ID', '')
 
 app = Flask(__name__, static_folder='static')
+bot = telebot.TeleBot(API_TOKEN_TELEBOT)
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        servico = request.form.get('servico')
+        mensagem = request.form.get('mensagem')
+
+        form_ = f"""Nome: {nome} \n Email: {email} \n serviço: {servico} \n mensagem: {mensagem}"""
+
+        bot.send_message(CHAT_ID, form_)
+
     return render_template('index.html')
+
 
 
 if __name__ == '__main__':
